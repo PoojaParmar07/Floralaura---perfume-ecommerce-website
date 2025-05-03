d9b6b74eaf89e40531854fd92509c6e63acda78c
Pooja Parmar <poojaparmar4014@gmail.com>
2025-05-03 14:43:58 +0530

    Add files via upload

---
+++ database/db.php
@@ -0,0 +1,16 @@
+<?php
+
+$host = "localhost";  
+$user = "root";       
+$pass = "";           
+$dbname = "floral_aura";
+
+// Create connection
+$conn = mysqli_connect($host, $user, $pass, $dbname);
+
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+?>
